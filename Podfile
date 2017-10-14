3d5521a425109081af689ba2f2d77c6b36d95190
chuross <azarasi.mo.mo@gmail.com>
2017-10-14 22:05:31 +0900

    add first screen

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 platform :ios, '10.0'
-use_frameworks!
 
 target 'qiiip' do
+  use_frameworks!
+  
   pod 'CHQiitaApiClient', :git => 'https://github.com/chuross/CHQiitaApiClient.git', :tag => '0.0.5'
 end
---
+++ qiiip/Sources/Ui/HomeScreenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeScreenViewController.swift
+//  qiiip
+//
+//  Created by chuross on 2017/10/14.
+//  Copyright © 2017年 chuross. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeScreenViewController : UIViewController {
+
+    
+}
