4f0a08a1128c1d72eeaa3f3fa16cac00aaa12b61
chuross <azarasi.mo.mo@gmail.com>
2017-10-14 16:41:32 +0900

    add initial files

---
+++ Pods/Target Support Files/CHQiitaApiClient/CHQiitaApiClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CHQiitaApiClient : NSObject
+@end
+@implementation PodsDummy_CHQiitaApiClient
+@end
---
+++ Pods/Target Support Files/Pods-qiiip/Pods-qiiip-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qiiip : NSObject
+@end
+@implementation PodsDummy_Pods_qiiip
+@end
