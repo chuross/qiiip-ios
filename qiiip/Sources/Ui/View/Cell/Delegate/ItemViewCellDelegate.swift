b49b41575a2d5897c822c9a34c0ab7661584777a
chuross <azarasi.mo.mo@gmail.com>
2017-10-21 23:06:09 +0900

    ItemViewCellとTableViewを繋ぐ部分を実装

---
+++ qiiip/Sources/Ui/View/Cell/Delegate/ItemViewCellDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  ItemViewCellDelegate.swift
+//  qiiip
+//
+//  Created by chuross on 2017/10/21.
+//  Copyright © 2017年 chuross. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CHQiitaApiClient
+
+class ItemViewCellDelegate: NSObject, UITableViewDelegate, UITableViewDataSource {
+    
+    private var cachedItems: [Item] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cachedItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: ItemViewCell = tableView.dequeueReusableCell(for: indexPath)
+        cell.setItem(item: self.cachedItems[indexPath.row])
+        return cell
+    }
+}
--- qiiip/Sources/Ui/View/Component/ItemsViewController.swift
+++ qiiip/Sources/Ui/View/Component/ItemsViewController.swift
@@ -13,8 +13,17 @@ import XLPagerTabStrip
 class ItemsViewController: UIViewController, IndicatorInfoProvider {
     
     @IBOutlet weak var listView: UITableView!
+    private var delegate: ItemViewCellDelegate? = nil
     
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return IndicatorInfo(title: "すべての投稿")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = ItemViewCellDelegate()
+            
+        self.listView.register(ItemViewCell.self)
     }
 }
