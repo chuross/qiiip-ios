9162c4300b80a860526c0daf2d691c4701e40ae0
chuross <azarasi.mo.mo@gmail.com>
2017-10-29 03:12:42 +0900

    ログイン用のViewControllerを追加

---
+++ qiiip/Sources/Ui/View/Modal/AuthorizeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizeViewController.swift
+//  qiiip
+//
+//  Created by chuross on 2017/10/29.
+//  Copyright © 2017年 chuross. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AuthirizeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
